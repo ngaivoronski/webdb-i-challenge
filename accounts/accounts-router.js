6065d4cf21b2a125c00da5ddf6d26d42eed5ffcb
ngaivoronski <ngaivoronski@gmail.com>
2019-12-09 17:55:59 -0500

    query string functionality added

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -5,13 +5,72 @@ const knex = require("../data/dbConfig.js");
 
 const router = express.Router();
 
+// validators
+
+function validateQuery(req, res, next) {
+    const validFields = ["name", "id", "budget"];
+    const validDirections = ["asc", "desc"];
+
+    // Everything is included
+    if (req.query && req.query.limit && req.query.sortby && req.query.sortdir) {
+        if (Number.isInteger(Number(req.query.limit)) && (req.query.limit > 0) && validFields.includes(req.query.sortby) && validDirections.includes(req.query.sortdir)) {
+            next();
+        }
+        else if (Number.isInteger(Number(req.query.limit)) && (req.query.limit > 0)) {
+            res.status(400).json({ message: "Please enter valid sorting parameters in your query string." });
+        }
+        else if (validFields.includes(req.query.sortby) && validDirections.includes(req.query.sortdir)) {
+            res.status(400).json({ message: "Please enter a positive integer for the limit in your query string." });
+        }
+        else {
+            res.status(400).json({ message: "Please enter valid sorting parameters in your query string and a positive integer for the limit in your query string." });
+        }
+    // Limit only included
+    } else if(req.query && req.query.limit) {
+        if (Number.isInteger(req.query.limit) && (req.query.limit > 0)) {
+            next();
+        } else {
+            res.status(400).json({ message: "Please enter a positive integer for the limit in your query string." });
+        }
+    // Sorting parameters only included
+    } else if (req.query && req.query.sortby && req.query.sortdir) {
+        if (validFields.includes(req.query.sortby) && validDirections.includes(req.query.sortdir)) {
+            next();
+        }
+        else {
+            res.status(400).json({ message: "Please enter valid sorting parameters in your query string." });
+        }
+    // Sorting category only included
+    } else if (req.query && req.query.sortby) {
+        if (validFields.includes(req.query.sortby)) {
+            next();
+        } else {
+            res.status(400).json({ message: "Please enter a valid sorting field." });
+        }
+    // Sort direction only
+    } else if (req.query && req.query.sortdir) {
+        res.status(400).json({ message: "Please enter a valid sorting field along with the sorting direction." });
+    }
+    // Nothing
+    else {
+        next();
+    }
+}
+
 // return a list of accounts from the database
 
-router.get("/", (req, res) => {
+router.get("/", validateQuery, (req, res) => {
     // select * from accounts
+    console.log(req.query);
+    const limit = req.query.limit;
+    const sortby = req.query.sortby;
+    const sortdir = req.query.sortdir;
+
     knex
         .select("*")
         .from("accounts")
+        .limit(limit)
+        .orderBy(sortby, sortdir)
         .then(accounts => {
         res.status(200).json(accounts);
         })
